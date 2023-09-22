e7501eeaab45ee846e43ea19b00ff39102a7298c
mlrprananta <lukeprananta@gmail.com>
2023-09-22 21:23:37 +0200

    Update workflow

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,5 @@
 import Main from '@/components/Main'
 import Hero from '@/components/Hero'
-import Position from '@/components/Position'
 import Navbar from '@/components/Navbar'
 import Experience from '@/components/Experience'
 import Footer from '@/components/Footer'
