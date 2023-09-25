e6a225fceb9a55a53aecf25201bdb5d3ba36a7d0
mlrprananta <lukeprananta@gmail.com>
2023-09-25 23:38:23 +0200

    Update background blur

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,8 +15,13 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html className="scroll-smooth antialiased select-none" lang="en">
-      <body className={inter.className}>{children}</body>
+    <html className="scroll-smooth antialiased select-none static" lang="en">
+      <body className={`${inter.className} bg-slate-900 `}>
+        {/* <div className="fixed mix-blend-normal -top-2/3 -left-1/3 w-2/3 h-full rounded-full bg-gradient-to-r from-emerald-800 to-indigo-800 -z-30 blur-3xl"></div> */}
+        <div className="fixed mix-blend-normal -top-3/4 -left-1/4 w-full h-full rounded-full bg-gradient-to-l from-emerald-800 from-20% to-slate-700 -z-30 blur-3xl"></div>
+        <div className="fixed mix-blend-normal -top-3/4 left-1/2 w-2/3 h-full rounded-full bg-gradient-to-r from-emerald-800 from-30% to-indigo-800 -z-30 blur-3xl"></div>
+        {children}
+      </body>
     </html>
   );
 }
