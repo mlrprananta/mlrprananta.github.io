e569fdb3b499e9e9db40fb9e5a37f2b9532743d7
mlrprananta <lukeprananta@gmail.com>
2023-09-26 10:51:55 +0200

    Update background colors

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,9 +16,9 @@ export default function RootLayout({
 }) {
   return (
     <html className="scroll-smooth antialiased select-none" lang="en">
-      <body className={`${inter.className} bg-slate-950`}>
-        <div className="absolute lg:fixed mix-blend-normal -top-1/4 left-1/4 lg:left-0 w-1/2 lg:w-2/3 h-2/3 rounded-b-full bg-gradient-to-t from-emerald-800/30 to-emerald-800/50 -z-30 blur-3xl"></div>
-        <div className="absolute lg:fixed mix-blend-normal -top-1/4 inset-0 w-full h-full bg-gradient-to-t from-slate-800/30 to-indigo-700/30 -z-40 blur-3xl"></div>
+      <body className={`${inter.className} bg-slate-900`}>
+        <div className="absolute lg:fixed mix-blend-normal -top-1/4 lg:left-0 left-1/4 w-1/2 lg:w-2/3 h-2/3 rounded-b-full bg-gradient-to-t from-emerald-800/40 to-emerald-800/60 -z-30 blur-3xl"></div>
+        <div className="absolute lg:fixed mix-blend-normal -top-1/4 inset-0 w-full h-full rounded-b-full lg:rounded-none bg-gradient-to-t from-slate-800/50 to-indigo-800/50 -z-40 blur-3xl"></div>
         {children}
       </body>
     </html>
