221e075c4cef4f5cc122c0011341f57e4becefbe
mlrprananta <lukeprananta@gmail.com>
2024-01-16 08:42:46 +0100

    Remove import

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import GoogleAnalytics from "@next/third-parties"
 import Script from "next/script";
 
 const inter = Inter({ subsets: ["latin"] });
